3ff683e1e4e7bbd81247773e6ffd3520750a7918
Pulkit Singhal <pulkitsinghal@gmail.com>
2012-05-27 11:15:41 -0500

    try to make the logic more robust

--- definitions/install_plugin.rb
+++ definitions/install_plugin.rb
@@ -5,15 +5,14 @@ define :install_plugin do
   bash "/usr/local/bin/plugin -install #{params[:name]}" do
     user "root"
     code "/usr/local/bin/plugin -install #{params[:name]}"
-
     notifies :restart, resources(:service => 'elasticsearch')
-
     not_if do
       Dir.entries("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/").any? do |plugin|
+      begin
         params[:name].include? plugin
-      end rescue false
+      rescue
+        false
+      end
     end
-
   end
-  
 end
